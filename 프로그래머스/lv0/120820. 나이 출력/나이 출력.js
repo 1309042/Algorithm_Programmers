05938767a3fa4551c9b70c020cb2273469edce6d
HyunSeo Oh <1309042@naver.com>
2023-05-13 12:08:59 +0900

    [level 0] Title: 나이 출력, Time: 3.94 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
@@ -0,0 +1,12 @@
+function solution(age) {
+    
+    let now = new Date();
+    let year = now.getFullYear();
+    
+    for(let i = 0; i < age; i++){
+      let result = year - age;
+        return result;
+    }
+    
+
+}
