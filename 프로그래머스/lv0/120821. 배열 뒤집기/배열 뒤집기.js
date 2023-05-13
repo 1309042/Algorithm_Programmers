ce1533ba33cd0fd1a5b342fe289cc3f69d6cd0f7
HyunSeo Oh <1309042@naver.com>
2023-05-13 18:14:25 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+
+    let reverseStr = [];
+    
+    for(let i = num_list.length - 1; 0 <= i; i--) {
+        reverseStr.push(num_list[i])
+
+    }
+     return reverseStr
+}
