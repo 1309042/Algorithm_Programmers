9b2fbb9845291a36726eb1f67c10cc949e0e653d
HyunSeo Oh <1309042@naver.com>
2025-02-18 20:07:22 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,19 @@
+n = input()  
+n_list = list(n)
+
+az_str = {
+    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 
+    'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 
+    'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 
+    'w': 23, 'x': 24, 'y': 25, 'z': 26
+}
+
+result = []
+
+for i in az_str.keys():
+    if i in n_list:  
+        result.append(n_list.index(i)) 
+    else:
+        result.append(-1) 
+
+print(" ".join(map(str, result)))
