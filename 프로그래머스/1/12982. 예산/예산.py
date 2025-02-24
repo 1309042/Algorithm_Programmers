961a82b13a94848071df932e98a80c84880667c2
HyunSeo Oh <1309042@naver.com>
2025-02-24 14:02:34 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,12 +1,10 @@
 def solution(d, budget):
-    d.sort()  #예산소트
-    count = 0  
-    
-    for i in d:
-        if budget >= i:  
-            budget -= i
-            count += 1
-        else:  
-            break
-            
-    return count
+    x = sorted(d)  
+    result = 0 
+    for i in range(len(d)):
+        if budget >= x[i]: 
+            budget -= x[i]  
+            result += 1  
+        else:
+            break  
+    return result 
